079e5e9be8a105b6165bed47ff94e543395906c7
Andrei Boghean <andreiboghean00@gmail.com>
2024-06-03 01:55:58 +0100

    try to load tiffs with webworkers

---
+++ plsWorker.js
@@ -0,0 +1,17 @@
+importScripts("geotiff.js")
+onmessage = async (something) => {
+	try {
+  const url = something.data;
+  const workerResult = `Result: ${url}`;
+  console.log("meester worker got", workerResult);
+  
+  
+  const tiff = await GeoTIFF.fromUrl(url);
+  const tiff_image = await tiff.getImage();
+  const data = await tiff_image.readRasters();
+  const boundingBox = tiff_image.getBoundingBox();
+   postMessage( [tiff_image, data, boundingBox] );
+	}
+	catch (e)
+	{ console.log("worker def:", e) }
+};
