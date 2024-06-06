3fd3a71307d9c2d7576a48ecaf770317772cd64d
Andrei Boghean <andreiboghean00@gmail.com>
2024-06-07 00:35:46 +0100

    cleanup

--- plsWorker.js
+++ plsWorker.js
@@ -2,16 +2,16 @@ import './geotiff.js';
 
 onmessage = async (something) => {
 	try {
-  const url = something.data;
-  const workerResult = `Result: ${url}`;
-  console.log("meester worker got", workerResult);
-  
-  const tiff = await GeoTIFF.fromUrl(url);
-  const tiff_image = await tiff.getImage();
-  const data = await tiff_image.readRasters();
-  const boundingBox = tiff_image.getBoundingBox();
-   postMessage( [tiff_image, data, boundingBox] );
+		const url = something.data;
+		const workerResult = `Result: ${url}`;
+		console.log("meester worker got", workerResult);
+
+		const tiff = await GeoTIFF.fromUrl(url);
+		const tiff_image = await tiff.getImage();
+		const data = await tiff_image.readRasters();
+		const boundingBox = tiff_image.getBoundingBox();
+		postMessage([tiff_image, data, boundingBox]);
+	} catch (e) {
+		console.log("worker def:", e)
 	}
-	catch (e)
-	{ console.log("worker def:", e) }
 };
