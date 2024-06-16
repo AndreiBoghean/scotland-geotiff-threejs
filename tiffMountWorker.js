509bf947f6e00579b7aaf16b1ea096f467b11d2a
Andrei Boghean <andreiboghean00@gmail.com>
2024-06-16 15:16:00 +0100

    raise north edge of map segments to help with orientation

--- tiffMountWorker.js
+++ tiffMountWorker.js
@@ -6,9 +6,14 @@ onmessage = async (something) => {
 		const tiffData = something.data[0]
 		const planeData = new Float32Array(something.data[1])
 
+		for (let ii = 0; ii <= 3601; ii++)
+			tiffData[0][ii] = 2000;
+
 		// set plane vertices to that of the tif data
 		for (let i = 0; i <= tiffData[0].length; i++)
-			planeData[i * 3 + 2] = tiffData[0][i];
+			planeData[i * 3 + 2] = tiffData[0][i] / 1000; // /1000 to convert height from meters to km (most of our lat/long calcs produce km and this is easier)
+		
+		
 		
 		postMessage(planeData.buffer, [planeData.buffer]);
 	} catch (e) {
